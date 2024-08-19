0a3236678f578e9febbe3157b5ddbd2b49aec5e2
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2024-08-19 22:59:07 +0300

    learned about vectors

--- vectors.cpp
+++ vectors.cpp
@@ -27,5 +27,49 @@ void add_entries(int n)
     // increase size by n
 }
 
+void add_over_range()
+{
+    // // int i = phone_book[1001].number;
+    // 1001 is out of range here
+    // this will add a random number apparently? let's see
+    // // cout << i;
+    // just made it 0
+
+    // throws an out_of_range apparently
+
+    try
+    {
+        int a = 0;
+        for (int i = 0; i < 1000; i++)
+        {
+            // using a here sets it to 0 all the time.
+            // // int a = 0;
+            print_entry(i);
+            a++;
+            cout << "the count is " << a;
+        }
+    }
+    catch (out_of_range)
+    {
+        cout << "range error\n";
+    }
+    // usual try-catch block
+    // using a main with a block like this is good practice
+}
+
 // copying large vectors and such is undesirable because of memory and compute
 // pointers and references is useful because of this
+
+int main(int argc, char const *argv[])
+{
+    try
+    {
+        add_over_range();
+    }
+    catch (...)
+    {
+        cerr << "unknown exception thrown\n";
+    }
+    // the ... handles any exception not caught before this
+    return 1;
+}
