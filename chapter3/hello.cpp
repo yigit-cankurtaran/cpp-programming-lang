e05576a4e401687fecaafdbc7bbed0df50ab19d9
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-02-11 13:27:00 +0300

    started chapter 3

---
+++ chapter3/hello.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // tell the compiler to include standard io stuff
+
+int main() { // main func, takes no args, does nothing.
+    std::cout << "hello world!\n"; // standard c out, put the string there
+    // if no problems are encountered the main func automatically returns 0
+}
