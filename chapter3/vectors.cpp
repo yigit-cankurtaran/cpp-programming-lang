a511619ecbda2253d547b09334256d924a8502a4
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-02-12 08:46:35 +0300

    learned about vectors

--- chapter3/vectors.cpp
+++ chapter3/vectors.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 
 struct Entry {
-    string name;
+    std::string name;
     int number;
 };
 
-vector<Entry> phone_book(1000);
+std::vector<Entry> phone_book(1000);
 
 void print_entry(int i) {
     std::cout << phone_book[i].name << ' ' << phone_book[i].number << '\n';
@@ -21,5 +21,17 @@ void add_entries(int n) {
 
 
 int main(){
+    phone_book[50].name = "hi";
+    phone_book[50].number = 50;
+    // most fundamental way to access and modify struct members
 
+    Entry entry1 = {.name = "hello", .number = 123};
+    // we can also create stuff like this
+    phone_book[51] = entry1;
+
+    Entry entry2 = {.name = "world", .number = 456};
+    phone_book.push_back(entry2); // push_back adds to the end of the vector
+    print_entry(50);
+    print_entry(51);
+    print_entry(1000);
 }
